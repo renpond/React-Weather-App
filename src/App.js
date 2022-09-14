5a35ec85e1c0b6b28e134d275558ab3d904bc977
renpond <renpond12@gmail.com>
2022-09-13 19:41:41 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,34 +5,36 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://reneepond-portfolio.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          Renee' Pond
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/renpond/React-Weather-App.git"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://ephemeral-yeot-0875d8.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://reneepond-portfolio.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Renee' Pond
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/renpond/React-Weather-App.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://ephemeral-yeot-0875d8.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
