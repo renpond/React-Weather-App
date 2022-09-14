615e878704452a4beadbeb5c149ce701d91496f1
renpond <renpond12@gmail.com>
2022-09-13 19:27:33 -0500

    Added rel to links

--- src/App.js
+++ src/App.js
@@ -7,15 +7,29 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded by Renee' Pond and is open-sourced on{" "}
+        This project was coded by{" "}
+        <a
+          href="https://reneepond-portfolio.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Renee' Pond
+        </a>{" "}
+        and is open-sourced on{" "}
         <a
           href="https://github.com/renpond/React-Weather-App.git"
           target="_blank"
+          rel="noopener noreferrer"
         >
           Github
         </a>{" "}
         and hosted on{" "}
-        <a href="https://ephemeral-yeot-0875d8.netlify.app/" target="_blank">
+        <a
+          href="https://ephemeral-yeot-0875d8.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Netlify
         </a>
       </footer>
