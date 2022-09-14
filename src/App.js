88062f87fcb84641fca12a0f099fadbd22396ba9
renpond <renpond12@gmail.com>
2022-09-13 19:02:46 -0500

    Created footer to add links to Github & Netlify

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Renee' Pond and is open-sourced on{" "}
+        <a
+          href="https://github.com/renpond/React-Weather-App.git"
+          target="_blank"
+        >
+          Github
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://ephemeral-yeot-0875d8.netlify.app/" target="_blank">
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
